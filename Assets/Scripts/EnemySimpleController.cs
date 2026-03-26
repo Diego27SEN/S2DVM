d14e5b4ae10364a634c424f08dc2dfdd21acf8d4
ChemsLGDesu <jamesgonelozano@gmail.com>
2026-03-26 00:05:40 -0500

    Hito N2
    Crear un enemigo terrestre que pueda perseguir al Player usando NavMeshAgent.

---
+++ Assets/Scripts/EnemySimpleController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySimpleController : MonoBehaviour
+{
+    public Transform Target;
+    private NavMeshAgent agentEnemy;
+
+    void Start()
+    {
+        agentEnemy = GetComponent<NavMeshAgent>();
+        Target = GameObject.FindGameObjectWithTag("Ball").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null && agentEnemy.isOnNavMesh)
+        {
+            agentEnemy.SetDestination(Target.position);
+
+            agentEnemy.speed = Random.Range(3f, 6f);
+            agentEnemy.acceleration = Random.Range(5f, 10f);
+            agentEnemy.stoppingDistance = Random.Range(1f, 3f);
+            agentEnemy.avoidancePriority = Random.Range(0, 99);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (agentEnemy == null || agentEnemy.path == null) return;
+
+        Vector3[] corners = agentEnemy.path.corners;
+
+
+        for (int i = 0; i < corners.Length - 1; i++)
+        {
+            Gizmos.DrawLine(corners[i], corners[i + 1]);
+            Gizmos.DrawSphere(corners[i], 0.2f);
+        }
+
+    }
+}
